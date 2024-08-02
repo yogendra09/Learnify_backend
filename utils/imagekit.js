738d72f03bc858a89d14b941967160068be5da92
shivam kumar shaha <shivamshah1210@gmail.com>
2024-08-02 11:38:52 +0530

    lernfiy-backend

---
+++ utils/imagekit.js
@@ -0,0 +1,13 @@
+// SDK initialization
+
+var ImageKit = require("imagekit");
+exports.intiImagekit = function (){
+
+    var imagekit = new ImageKit({
+        publicKey : process.env.PUBLICKEY_IMAGEKIT,
+        privateKey : process.env.PRIVATEKEY_IMAGEKIT,
+        urlEndpoint : process.env.ENDPOINT_IMAGEKIT
+    });
+
+    return imagekit;
+}
